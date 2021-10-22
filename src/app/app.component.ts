bab42c77cdb178a863ce46ca23cf922793d49634
eceyzgn <eceyyazgan@gmail.com>
2021-10-22 22:35:10 +0300

    Adding Item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Model } from './model';
+import { Model, TodoItem} from './model';
 
 @Component({
   selector: 'app-root',
@@ -16,5 +16,11 @@ export class AppComponent {
  
   getItems(){
     return this.model.items.filter(item => !item.action);
+  }
+
+  addItem(value){
+    if(value!=""){
+      this.model.items.push(new TodoItem(value,false));
+    }
   }
 }
